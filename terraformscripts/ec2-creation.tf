c72e282116523773de3358f8b0d62c81f7841ff1
shiva09718 <112337335+shiva09718@users.noreply.github.com>
2023-10-27 15:32:19 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,12 +1,12 @@
 provider "aws" {
 region="ap-south-1"
 }
-resource "aws_instance" "AWSinstance" {
+resource "aws_instance" "AWSInstance" {
  ami = "ami-06791f9213cbb608b"
  instance_type = "t2.micro"
  key_name = "devopsmain2023"
  security_groups = ["launch-wizard-2"]
  tags = {
- name = "teraform serv"
+ name = "teraformserv"
  }
 }
